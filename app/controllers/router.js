733da7100713ee0775f2d88fd763cc0948f5ce8c
MuharremElyesa <muharremelyesa1453@gmail.com>
2024-01-09 12:07:32 +0300

    Devamke ;)

--- app/controllers/router.js
+++ app/controllers/router.js
@@ -1,5 +1,4 @@
 /* Moduller */
-
 // Express.js'i tanımladık:
 const express = require("express")
 // express'in Router metodunu tanımladık:
@@ -7,11 +6,15 @@ const router = express.Router()
 // Firebase bağlantısı:
 const firebase = require("../../config/firebase-connect")
 
+
+/* Program Akışı */
 // Ana dizin middleware:
 router.use("/",(req,res)=>{
-    // Express ile "open-login.pug" dosyasını response(göndermek) ediyoruz:
+    // Express ile "open-login.pug" dosyasını response (göndermek) ediyoruz:
     res.render("open-login")
 })
 
+
+/* Export */
 // router'i dış kullanıma açıyoruz:
 module.exports = router
