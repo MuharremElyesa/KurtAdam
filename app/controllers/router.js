b9da2ac23e686c5fcb1200905d2a79a37ccbb9cf
MuharremElyesa <muharremelyesa1453@gmail.com>
2024-02-21 01:39:45 +0300

    geçici isim bağlantısı

--- app/controllers/router.js
+++ app/controllers/router.js
@@ -3,16 +3,20 @@
 const express = require("express")
 // express'in Router metodunu tanımladık:
 const router = express.Router()
+// express app:
+const app = express()
+// FirebaseAdmin:
+const firebaseAdmin = require("./../../config/firebase-connect")
+// db--nick-name:
+const db__nick_name = require("./../models/db--nick-name")
+
 
 /* Program Akışı */
-
-// Oturum Açmak:
-router.use("/GoogleSignIn",(req,res)=>{
-    res.send("deneme")
-})
+// db__nick_name:
+router.use(db__nick_name)
 
 // Ana dizin middleware:
-router.use("/",(req,res)=>{
+router.use("/", (req, res) => {
     // Express ile "open-login.pug" dosyasını response (göndermek) ediyoruz:
     res.render("open-login")
 })
