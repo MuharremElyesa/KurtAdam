ff1bb1f0dfc71ed76a7c24f1a7f2b23a4abe8957
MuharremElyesa <muharremelyesa1453@gmail.com>
2024-03-10 01:32:19 +0300

    fotoğraf eklendi ve düzenlendi

--- config/global-variables.js
+++ config/global-variables.js
@@ -1,2 +1,12 @@
 // Oyun Adı:
 exports.gameName = "KurtAdam"
+// Oyuncu ID:
+exports.playerID
+// Oyuncunun Adı:
+exports.playerName
+// Oyuncunun Soyadı:
+exports.playerSurname
+// Oyuncunun Tam Adı:
+exports.playerFullname
+// Oyuncunun Profil Fotoğrafı:
+exports.playerPhoto
