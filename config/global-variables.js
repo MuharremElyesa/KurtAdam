c50822ea1e344dee1294276b44e4e96301e13287
MuharremElyesa <muharremelyesa1453@gmail.com>
2024-04-05 03:54:03 +0300

    socket.io ile server kısmında yapılan değişiklik ile oda oluşturma ve veritabanı değişiklerini izleme olayı eklendi.

--- config/global-variables.js
+++ config/global-variables.js
@@ -1,15 +1,23 @@
-// Oyun Adı:
-exports.gameName = "KurtAdam"
-// Oyuncu ID:
-exports.playerID
-// Oyuncunun Adı:
-exports.playerName
-// Oyuncunun Soyadı:
-exports.playerSurname
-// Oyuncunun Tam Adı:
-exports.playerFullname
-// Oyuncunun Profil Fotoğrafı:
-exports.playerPhoto
+// Oyun
+    // Oyun Adı:
+    exports.gameName = "KurtAdam"
+    // Rastgele oda kimliği:
+    exports.randomRoomKey
 
-// Rastgele oda kimliği:
-exports.randomRoomKey
+
+// Profil Bilgileri
+    // Oyuncu ID:
+    exports.playerID
+    // Oyuncunun Adı:
+    exports.playerName
+    // Oyuncunun Soyadı:
+    exports.playerSurname
+    // Oyuncunun Tam Adı:
+    exports.playerFullname
+    // Oyuncunun Profil Fotoğrafı:
+    exports.playerPhoto
+
+
+// Fonksiyonlar
+    // game-creating.js:
+        exports.preGamePlayerListRefresh
