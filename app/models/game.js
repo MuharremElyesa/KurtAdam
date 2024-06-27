50041b6203b7bded4ddceac05064d9cfef233e5c
MuharremElyesa <muharremelyesa1453@gmail.com>
2024-06-27 04:30:25 +0300

    Rol gösterme ekranı yazılım aşamasında...

--- app/models/game.js
+++ app/models/game.js
@@ -13,6 +13,10 @@ globalVariables.timeQueryFunction = function(io, clientID, data) {
         case "forTheGameToStart":
             clientRequest = "toTheBeginningOfTheGame"
             break;
+
+        case "showingRoles":
+            clientRequest = "showingRoles"
+            break;
     
         default:
             break;
@@ -34,11 +38,87 @@ globalVariables.listContats = function(io, clientID, data) {
 
 // Rol dağıtımı:
 globalVariables.roleDistribution = function(io, clientID, data) {
+
+    // Fonksiyon içi değişkenler:
+    var players=[]
+    var roles=[]
+
     // Bundan önceki kontrol bitirilir:
     firebaseAdmin.database().ref("roomKeys/"+data.enteredRoomKey).once("value", (snapshot)=>{
         firebaseAdmin.database().ref("roomKeys/"+data.enteredRoomKey+"/gameConfig").update({
             toTheBeginningOfTheGameControl: true
         })
-        // Buradan sonra rol dağıtımı yapılacak ve devam edilecek...
+
+        // Odadaki kişi sayısını alıyoruz:
+        for (let i = 0; i < Object.keys(snapshot.val()).length; i++) {
+            if (Object.keys(snapshot.val())[i]=="gameConfig") {
+                continue
+            }
+            // Odada bulunun oyuncuların ID'lerini bir diziye alıyoruz. Bunu yaptıktan sonra kaç kişi olduğunu da tespit edebiliyoruz:
+            players.push(Object.keys(snapshot.val())[i])
+        }
+
+        // Global-variables'deki roller objesini getirip rolleri diziye aktarıyoruz:
+        Object.keys(globalVariables.roles).forEach(element => {
+            roles.push(element)
+        })
+
+        // Kaç tane kurt atanacağını belirliyoruz:
+        var number_of_wolves = Math.floor(players.length / globalVariables.how_many_people_have_a_wolf)
+
+        // Atanmış roller dizisi (Kurt sayısı kadar assignedRoles dizisine "wolf" yazdırıyoruz.):
+        var assignedRoles = new Array(number_of_wolves).fill("wolf")
+
+        // Geriye kaç kişi kaldıysa, o kadar köylü rolu atıyoruz (ŞİMDİLİK):
+        var number_of_people_remaining = players.length - number_of_wolves
+
+        // Diğer rolleri rastgele bir şekilde dağıtıyoruz  (Şu anda sadece KÖYLÜ):
+        var remaining_roles = new Array(number_of_people_remaining).fill("villager")
+        // for (let i = 0; i < number_of_people_remaining; i++) {
+        //     var randomRole = roles[Math.floor(Math.random()*roles.length)]
+        //     remaining_roles.push(randomRole)
+        // }
+
+        // Kurtlar ve diğer rolleri tek dizide birleştiriyoruz:
+        var all_roles = assignedRoles.concat(remaining_roles)
+
+        // Rolleri karıştırmak için Fisher-Yates algoritmasını kullanıyoruz:
+        shuffleArray(all_roles)
+
+        // Kişileri karıştırmak için Fisher-Yates algoritmasını kullanıyoruz:
+        shuffleArray(players)
+
+        // Kullanıcı idlerine rolleri değıtıyoruz:
+        var user_roles = {}
+        players.forEach((id, index) => {
+            user_roles[id] = all_roles[index]
+        })
+
+        // Atanan rolleri veritabanında kişilere yazdırıyoruz:
+        for (let i = 0; i < players.length; i++) {
+            firebaseAdmin.database().ref("roomKeys/"+data.enteredRoomKey).child(Object.keys(user_roles)[i]).update({
+                role: Object.values(user_roles)[i]
+            })
+        }
+
+        // Rol dağıtım mevzusu bittikten sonra rolün ekranda gösterileceği süreyi giriyoruz:
+        firebaseAdmin.database().ref("roomKeys/"+data.enteredRoomKey+"/gameConfig").update({
+            showingRoles: timeKeeper(globalVariables.showing_roles),
+            showingRolesControl: false
+        })
+
     })
+}
+
+// Dizileri karıştırma işlevi
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+// Şimdiki zamanın üstüne zaman koymamızı sağlayan fonksiyon:
+function timeKeeper(/*Koyulacak zaman*/ timeToBePlaced) {
+    return Math.floor(Date.now() / 1000) + timeToBePlaced
 }
--- config/global-variables.js
+++ config/global-variables.js
@@ -1,19 +1,29 @@
 // Oyun
     // Oyun Adı:
-    exports.gameName = "KurtAdam"
+        exports.gameName = "KurtAdam"
     // Rastgele oda kimliği:
-    exports.randomRoomKey
+        exports.randomRoomKey
     // open-login.pug sayfasındaki takma isim kısmındaki yasaklı karakterler:
-    exports.bannedCharacters1 = "< > / \\ - + = ."
+        exports.bannedCharacters1 = "< > / \\ - + = ."
     // Rastgele oyuncu id'si oluşturma alt ve üst değeri:
-    exports.r_id_min = 1000000
-    exports.r_id_max = 9999999
-    // exports.r_id_length = this.r_id_max.toString().length
+        exports.r_id_min = 1000000
+        exports.r_id_max = 9999999
+        // exports.r_id_length = this.r_id_max.toString().length
     // Rastgele oda kimliği alt ve üst değerleri:
-    exports.r_room_min = 100000
-    exports.r_room_max = 999999
-    // Oyunun başlamasına kalan süre:
-    exports.time_left_until_the_game_starts = 10
+        exports.r_room_min = 100000
+        exports.r_room_max = 999999
+    // Roller (Bu kısımda sol tarafta roller, sağ tarafta o rölün hangi takımdan olacağı yer alıyor.):
+        exports.roles = {
+            villager: "village",
+            wolf: "wolf"
+        }
+    // Kaç kişide bir kurt rolu atanacağını belirliyoruz:
+        exports.how_many_people_have_a_wolf = 5
+    // Oyundaki süre değerleri:
+        // Oyunun başlamasına kalan süre:
+            exports.time_left_until_the_game_starts = 3 /*10*/
+        // Rollerin gösterilme süresi
+            exports.showing_roles = 10000
     
 
 // Fonksiyonlar
